d8f125b4bc2e860afaa7aee8a3321fcc3824985f
AlenaAbaseva <abasevaa@gmail.com>
2025-02-06 11:28:16 +0200

    Viikkotehtava 5

---
+++ Notifikaattori/notifikaattori.cpp
@@ -0,0 +1,56 @@
+#include "notifikaattori.h"
+
+Notifikaattori::Notifikaattori(): seuraajat (nullptr){
+    cout<<"Luodaan notifikaattori"<<endl;
+}
+
+Notifikaattori::~Notifikaattori(){
+
+}
+
+void Notifikaattori::lisaa(Seuraaja* seuraaja){
+    seuraaja->next = seuraajat;
+    seuraajat = seuraaja;
+    cout<<"Notifikaattori lisaa seuraajan "<<seuraaja->getNimi()<<endl;
+}
+
+void Notifikaattori::poista(Seuraaja* seuraaja){
+    Seuraaja* nykyinen = seuraajat;
+
+    if (nykyinen == seuraaja){
+        seuraajat = seuraaja->next;
+        delete seuraaja;
+        cout<<"Notifikaattori poistaa seuraajan "<<seuraaja->getNimi()<<endl;
+        return;
+    }
+
+    while (nykyinen != nullptr) {
+        if (nykyinen->next == seuraaja) {
+            nykyinen->next = seuraaja->next;
+            delete seuraaja;
+            cout << "Notifikaattori poistaa seuraajan " << seuraaja->getNimi() << endl;
+            return;
+        }
+        nykyinen = nykyinen->next;
+    }
+}
+
+void Notifikaattori::tulosta() {
+    Seuraaja* nykyinen = seuraajat;
+    cout<<"Notifikaattori seuraajat:"<<endl;
+
+    while (nykyinen != nullptr) {
+        cout<< "Seuraaja "<< nykyinen->getNimi()<< endl;
+        nykyinen = nykyinen->next;
+    }
+}
+
+void Notifikaattori::postita(string viesti){
+    Seuraaja* nykyinen = seuraajat;
+
+    cout<<"Notifikaattori postaa viestin "<< viesti<<endl;
+    while (nykyinen != nullptr) {
+        nykyinen->paivitys(viesti);
+        nykyinen = nykyinen->next;
+    }
+}
